ffe1b2b5f3b128fffe32c6446a34563a9b3fd022
Miguel Gomez <miguel5121996@gmail.com>
2023-10-09 18:34:40 +0200

    Realizacion de ventanas y binding

--- VariosActivitysConDatos/app/build.gradle.kts
+++ VariosActivitysConDatos/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.variosactivityspasodatos"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.variosactivityspasodatos"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
 
--- VariosActivitysConDatos/app/src/main/java/com/example/variosactivityspasodatos/MainActivity.kt
+++ VariosActivitysConDatos/app/src/main/java/com/example/variosactivityspasodatos/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.variosactivityspasodatos
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivityspasodatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.boton.setOnClickListener{
+            irAVentana2()
+        }
+    }
+
+    private fun irAVentana2() {
+        var miIntent : Intent = Intent(this,Ventana2::class.java)
+        startActivity(miIntent)
     }
 }
